1a29089208358822ab26fd4ed5f24c531aede3e7
EthannSchneider <ethann@shkermit.ch>
2022-12-02 08:24:38 +0100

    Added cogs

---
+++ cogs/Ping.py
@@ -0,0 +1,12 @@
+import discord
+
+class Ping(discord.Cog):
+    def __init__(self, bot) -> None:
+        self._bot = bot
+
+    @discord.slash_command()
+    async def ping(self, ctx, *, search: str):
+        await ctx.respond("pong")
+
+def setup(bot):
+    bot.add_cog(Ping(bot))
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 import discord
 from libs.db import DbConnector
+import os
 
 bot = discord.Bot()
 
@@ -10,15 +11,15 @@ TOKEN = None
 with open("key.txt","r") as key:
     TOKEN = key.readline()
 
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
 @bot.event
 async def on_ready():
     print('Logged in as')
     print(bot.user)
     print(bot.user.id)
     print('------')
-
-@bot.slash_command(name="ping",description="Say pong")
-async def Ping(ctx):
-    await ctx.respond("pong")
 
 bot.run(TOKEN)
