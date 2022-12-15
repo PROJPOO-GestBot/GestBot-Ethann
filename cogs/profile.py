415844be1ad2047a59f557a2af11274c13f9ab7a
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2022-12-15 22:46:57 +0100

    End of Review Sprint 01

--- cogs/profile.py
+++ cogs/profile.py
@@ -1,34 +1,40 @@
-import discord, os
+import discord
+import os
+
 from lib.imageMaker import ProfilImage
 
-class profil(discord.Cog):
+
+class Profile(discord.Cog):
     def __init__(self, bot) -> None:
         self._bot = bot
 
-    @discord.slash_command(name="profil", description="Return your profil on server")
-    async def profil(self, ctx):
+    @discord.slash_command(name="profile", description="Return your profile on server")
+    async def profile(self, ctx):
         await ctx.defer()
 
-        imgProfil = "img/profil/"
-        imgBackground = "img/background/"
+        # TODO this folder doesn't exist
+        img_profile = "img/profile/"
+        img_background = "img/background/"
 
-        self.__makeDirs(imgProfil)
-        self.__makeDirs(imgBackground)
+        self.__makeDirs(img_profile)
+        self.__makeDirs(img_background)
 
+        #TODO Review Remove data from you code. You must using parameters to pass this kind of information.
         pro = ProfilImage(
-            imgProfil+"386200134628671492.png",
+            img_profile + "386200134628671492.png",
             "Ethann8#7747",
             "https://cdn.discordapp.com/avatars/386200134628671492/a_de9e9a4c0e60276e7252e9b75c821b49.png",
             5,
             420,
             "Ethann",
-            background=imgBackground+"default"
+            background=img_background + "default"
         )
         await ctx.respond(file=discord.File(pro.ProfilPath()))
 
-    def __makeDirs(self,dirs):
+    def __make_dirs(self, dirs):
         if not os.path.exists(dirs):
             os.makedirs(dirs)
 
+
 def setup(bot):
-    bot.add_cog(profil(bot))
+    bot.add_cog(profile(bot))
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-#!/usr/local/bin/python3
+import os
 import discord
-from lib.db import Database
-import os,csv
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -21,11 +19,13 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
+
 @bot.event
 async def on_ready():
     print('Logged in as')
     print(bot.user)
     print(bot.user.id)
     print('------')
+
 
 bot.run(os.getenv("BOT_TOKEN"))
